f616fd16e9bbca843b9d01522471641d7b61f599
Kent Martin Åsvang <github.com@asvang.no>
2024-04-13 18:51:19 +0200

    refactored cf_client

--- cf_client.py
+++ cf_client.py
@@ -2,35 +2,83 @@ import pprint
 import requests
 import settings
 
+from datetime import datetime
+from logger import logger
+
+from pydantic import BaseModel
+from typing import Dict, List, Any, Optional
+
+class CloudflareDNSRecord(BaseModel):
+    content: str
+    created_on: datetime 
+    id: str
+    locked: bool
+    meta: Dict[str, bool]
+    modified_on: datetime 
+    name: str
+    proxiable: bool
+    proxied: bool
+    tags: List[Any]
+    ttl: int 
+    type: str
+    zone_id: str
+    zone_name: str
+
+class CloudflareResponse(BaseModel):
+    errors: List[Any]
+    messages: List[Any]
+    result: List[CloudflareDNSRecord] | CloudflareDNSRecord
+    result_info: Optional[Dict[str, int]] = None
+    success: bool
+
+class UpdateDNSRecord(BaseModel):
+    content: str
+    name: str
+    type: str
+    comment: str
+    proxied: bool
 
 class CFClient:
-    base_url = "https://api.cloudflare.com/client/v4/"
+
+    BASE_URL = "https://api.cloudflare.com/client/v4/"
+
     def __init__(self, auth_key, zone_id):
+        logger.debug("Initializing CFClient")
         self.auth_key = auth_key
         self.zone_id = zone_id
 
-    def get_dns_records(self):
-        url = self.base_url + "zones/" + self.zone_id + "/dns_records"
-        result = requests.get(url, headers={"Authorization": "Bearer " + self.auth_key})
-        json_result = result.json()
-        return json_result
+    def get_dns_records(self) -> List[CloudflareDNSRecord] | None:
+        url = self.BASE_URL + "zones/" + self.zone_id + "/dns_records"
+        params = {"type": "A"}
+        response = requests.get(url, params=params, headers={"Authorization": "Bearer " + self.auth_key})
+        cf_response = CloudflareResponse(**response.json())
 
-    def change_ip(self, domain, ip):
-        dns_record_id = self.get_record_id(domain)
-        url = self.base_url + "zones/" + self.zone_id + "/dns_records/" + dns_record_id
-        result = requests.put(url, headers={"Authorization": "Bearer " + self.auth_key}, json={"name": domain, "type": "A", "content": ip})
-        json_result = result.json()
-        return json_result
+        if response.status_code == requests.codes.ok and cf_response.success:
+            return cf_response.result
 
-    def get_record_id(self, domain):
-        dns_records = self.get_dns_records()
-        for record in dns_records["result"]:
-            if record["name"] == domain:
-                return record["id"]
         return None
 
+    def update_ip_address(self, dns_record: CloudflareDNSRecord, new_ip) -> bool:
+        url = self.BASE_URL + "zones/" + dns_record.zone_id + "/dns_records/" + dns_record.id
 
-if __name__ == "__main__":
-    client = CFClient(settings.AUTH_KEY, settings.ZONE_ID)
-    ret = client.change_ip("test.hsomsorg.no", "0.0.0.0")
-    pprint.pprint(ret)
+        update_record = UpdateDNSRecord(
+            content=new_ip, 
+            proxied=dns_record.proxied,
+            name=dns_record.name, 
+            type=dns_record.type, 
+            comment=f"Updated by pynamic_dns on '{datetime.now().isoformat()}'"
+            )
+
+        response = requests.put(
+            url, 
+            headers={"Authorization": "Bearer " + self.auth_key}, 
+            json=update_record.model_dump()
+            )
+
+        cf_response = CloudflareResponse(**response.json())
+
+        if response.status_code == requests.codes.ok and cf_response.success:
+            return True
+
+        return False
+
