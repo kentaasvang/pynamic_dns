80e5b689a5d51c9a50a8f3614b81e98d5354704d
Kent Martin Åsvang <github.com@asvang.no>
2024-04-13 18:53:55 +0200

    finished v1

--- main.py
+++ main.py
@@ -1,55 +1,53 @@
 #!./venv/bin/python3.12
-
 import os
+import sys
 import time
 import requests
 import settings
-import logging
+from datetime import datetime
+from logger import logger
+from pprint import pprint
 
-from cf_client import CFClient
+from cf_client import CFClient, CloudflareDNSRecord
 
 def main():
+    logger.info("Starting pynamic_dns")
 
-    if not settings.CURRENT_IP:
-        ipv4_file_path = os.path.join("data", "IPV4.txt")
+    # get current public server ip
+    current_server_ip = get_current_server_ip()
+    logger.debug(f"current_server_ip: '{current_server_ip}'")
 
-        if os.path.exists(ipv4_file_path):
-            with open(ipv4_file_path, "r") as ipv4_file:
-                print(f"setting ip-address from {ipv4_file_path}")
-                CURRENT_IP = ipv4_file.read()
-                print(f"current IP is {CURRENT_IP}")
-        else:
-            CURRENT_IP = get_current_server_ip()
-            with open(ipv4_file_path, "w") as ipv4_file:
-                print(f"writing IP address to {ipv4_file_path}")
-                ipv4_file.write(CURRENT_IP)
-                print(f"current IP is {CURRENT_IP}")
+    cf_client = CFClient(settings.AUTH_KEY, settings.ZONE_ID)
 
+    # get current ip from DNS
+    dns_records: List[CloudflareDNSRecord] = cf_client.get_dns_records()
+
+    for dns_record in dns_records:
+        logger.info(f"Checking IP on '{dns_record.name}'")
+
+        if dns_record.name not in settings.domains:
+            logger.info(f"Skipping record: '{dns_record.name}'")
+            continue
+
+        if dns_record.content != current_server_ip:
+            logger.info(f"Updating IP on '{dns_record.name}' to '{current_server_ip}'")
+            result = cf_client.update_ip_address(dns_record, current_server_ip)
+
+            if result:
+                logger.info(f"Successfully updated '{dns_record.name}'")
+            else:
+                logger.warning(f"Something wen't wrong updating dns_record '{dns_record.name}'")
+
+        logger.info(f"Finished checking '{dns_record.name}'")
+
+    logger.info("Exiting")
+    exit(0)
+    
 
 def get_current_server_ip():
     result = os.popen("curl -4 ifconfig.me").read()
     return result
 
-cf_client = CFClient(settings.AUTH_KEY, settings.ZONE_ID)
-
-"""
-while True:
-    # Get the current IP address
-    current_ip = get_current_server_ip()
-    print(current_ip)
-
-    dns_records = cf_client.get_dns_records()
-    print(dns_records)
-    for dns_record in dns_records["result"]:
-        if dns_record["name"] in settings.domains:
-            if dns_record["content"] != current_ip:
-                print("Changing IP for", dns_record["name"])
-                res = cf_client.change_ip(dns_record["name"], current_ip)
-            else:
-                print("IP for", dns_record["name"], "is already correct")
-    # Sleep for 6 hours
-    time.sleep(2000)
-"""
 
 if __name__ == "__main__":
     main()
--- settings.py
+++ settings.py
@@ -2,8 +2,27 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+from logging import (
+        DEBUG,
+        INFO,
+        WARNING
+)
+
+from datetime import datetime
+
+# WORKER 
+SLEEP_IN_SECONDS=5
+
+
+# CLOUDFLARE
 ZONE_ID = os.getenv("ZONE_ID")
 AUTH_KEY = os.getenv("AUTH_KEY") 
-
-# add domains to track here
 domains = [] 
+
+
+# LOGGER
+# writing to file:
+#LOG_FILE = os.path.join("logs", f"{datetime.now().isoformat()} pynamic_dns.log")
+LOG_LEVEL = INFO
+LOG_FILE = None
+
